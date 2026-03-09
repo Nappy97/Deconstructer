00720092739ca3fe339e1d3d4082a35004bf2ab2
UihyungCheong <uihyung.cheong@farmkit.kr>
2026-03-09 09:20:05 +0900

    Initial commit

---
+++ DeconstructerGenerator/Method.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace DeconstructerGenerator;
+
+public class Method(string modifier, string attribute, string name, string returnType, List<Parameter> parameters, string code)
+{
+    public string AsyncReturnType => ReturnType switch
+    {
+        "void" => "Task",
+        _ => $"Task<{ReturnType}>"
+    };
+
+    public string Modifier { get; } = modifier;
+    public string Attribute { get; } = attribute;
+    public string Name { get; } = name;
+    public string ReturnType { get; } = returnType;
+    public List<Parameter> Parameters { get; } = parameters;
+    public string Code { get; } = code;
+
+    public override string ToString()
+    {
+        StringBuilder builder = new ();
+        builder.AppendLine($"{AsyncReturnType} {Name}");
+        foreach (var parameter in Parameters)
+            builder.AppendLine($"\t{parameter}");
+
+        return builder.ToString();
+    }
+    
+    public string ToAsyncText()
+    {
+        // public Task SleepAsync(int seconds, int milliSeconds) => Task.Run(() => Sleep(seconds, milliSeconds));
+        // public Task<List<int>> GetEvensAsync()  => Task.Run(() => GetEvens());
+
+        string typeAndNames = string.Join(", ", Parameters);
+        string names = string.Join(", ", Parameters.Select(x => x.Name));
+
+        return $"""
+                {Modifier} {AsyncReturnType} {Name}Async({typeAndNames}) => Task.Run(() => {Name}({names}));
+                """;
+    }
+}
+
+public class Parameter(string type, string name, EqualsValueClauseSyntax argDefault)
+{
+    public string Type { get; } = type;
+    public string Name { get; } = name;
+    public EqualsValueClauseSyntax ArgDefault { get; } = argDefault;
+
+    public override string ToString()
+    {
+        return ArgDefault switch
+        {
+            null => $"{Type} {Name}",
+            _ => $"{Type} {Name} = {ArgDefault.Value}",
+        };
+    }
+}
---
+++ DeconstructerGenerator/MethodParser.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace DeconstructerGenerator;
+
+public class MethodParser
+{
+    public static List<Method> Parse(string code)
+    {
+        var syntaxTree = CSharpSyntaxTree.ParseText(code);
+        var root = syntaxTree.GetRoot() as CompilationUnitSyntax;
+
+        var span = root.Usings.ToString();
+        Console.WriteLine(span);
+
+        var methodDeclarations = root!.DescendantNodesAndSelf().OfType<MethodDeclarationSyntax>().ToList();
+
+        if (methodDeclarations.Any())
+        {
+            return methodDeclarations.Select(x => ParseMethod(x, code)).ToList();
+        }
+        else
+        {
+            code = WrapIntoClass(code);
+            return Parse(code);
+        }
+    }
+
+    private static Method ParseMethod(MethodDeclarationSyntax syntax, string code)
+    {
+        var parameters = syntax.ParameterList.Parameters.Select(
+            x => new Parameter(FromTypeSyntax(code, x.Type!), x.Identifier.Text, x.Default)
+        );
+
+        var attributes = string.Join("\r\n", syntax.AttributeLists.Select(x => x.ToString()));
+        
+        Method method = new Method(syntax.Modifiers.ToString(), attributes, syntax.Identifier.Text, FromTypeSyntax(code, syntax.ReturnType), parameters.ToList(), syntax.ToString());
+
+        return method;
+    }
+
+    public static string WrapIntoClass(string snippet) => 
+        $$"""
+          class _{{Guid.NewGuid():N}}
+          {
+                  {{snippet}}
+          }
+          """;
+
+    public static string FromTypeSyntax(string code, TypeSyntax syntax)
+        => code.Substring(syntax.Span.Start, syntax.Span.End - syntax.Span.Start);
+}
